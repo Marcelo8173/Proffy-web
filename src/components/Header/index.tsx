eb0216066930f0fe21c13d2822800917d4058a74
Marcelo_André <masn.hef2@uea.edu.br>
2020-08-03 17:23:30 -0400

    page header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import './styles.css';
 import Logo from '../../assets/images/logo.svg';
 import BackIcon from '../../assets/images/icons/back.svg'
 import { Link } from 'react-router-dom';
-
+import './styles.css';
 interface HeaderProps{
     title: string;
 }
 
-const PageHeader:React.FC<HeaderProps> = ({title}) =>{
+const PageHeader:React.FC<HeaderProps> = ({title,children}) =>{
     return(
         <>
         <header className="page-header">
@@ -21,6 +21,7 @@ const PageHeader:React.FC<HeaderProps> = ({title}) =>{
 
             <div className="header-content">
                 <strong>{title}</strong>
+                {children}
             </div>
         </header>
     </>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -7,7 +7,24 @@ import PageHeader from '../../components/Header';
 const TeacherList:React.FC = () =>{
     return(
         <div id="page-teacher-list" className="container">
-           <PageHeader title="Esses são os proffys dispiníveis." />
+           <PageHeader title="Esses são os proffys dispiníveis.">
+               <form id="search-teacher">
+                   <div className="input-block">
+                       <label htmlFor="subject">Máteria</label>
+                       <input type="text" id="subject"/>
+                   </div>
+
+                   <div className="input-block">
+                       <label htmlFor="week-day">Dia da semana</label>
+                       <input type="text" id="week-day"/>
+                   </div>
+
+                   <div className="input-block">
+                       <label htmlFor="time">Hora</label>
+                       <input type="text" id="time"/>
+                   </div>
+               </form>
+           </PageHeader>
         </div>
     )
 }
